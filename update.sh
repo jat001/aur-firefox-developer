79339aa85af0069fdccbb41cf849ba73aa8b697a
Jat <jat@sinosky.org>
2017-10-16 00:57:06 +0800

    c

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # author: chat@jat.email
 
 function error () {
-    printf "==> ERROR: $1\n" "${@:2}" >&2
+    echo "$@" >&2
 }
 
 function die () {
@@ -16,7 +16,11 @@ version=$(curl $api 2>/dev/null | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/n
 [ -z "$version" ] && die 'Get the latest version of firefox-developer error.'
 
 for package in packages/*; do
-    pkgbuild="packages/$package/PKGBUILD"
+    pkgbuild="$package/PKGBUILD"
+
+    pkgver=$(source "$pkgbuild"; echo "$pkgver")
+    echo "$pkgver"
+    die 'test'
 
     [ -z "$pkgver" ] && error "Cannot get the current version of $package, ignoring." && continue
     [ "$pkgver" == "$version" ] && error "The current version of $package is the latest, ignoring." && continue
