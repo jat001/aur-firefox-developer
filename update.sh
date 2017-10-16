c0adc19da91d8ca7aeba534252a1b63b2dafed54
Jat <jat@sinosky.org>
2017-10-16 18:44:08 +0800

    bugfix

--- update.sh
+++ update.sh
@@ -20,20 +20,22 @@ sha512sums=$(curl "https://download-installer.cdn.mozilla.net/pub/devedition/rel
 [ -z "$sha512sums" ] && die 'Get sha512sums of firefox-developer error.'
 
 for package in packages/*; do
-    package=${package#*/}
-    pkgbuild="packages/$package/PKGBUILD"
-    srcinfo="packages/$package/.SRCINFO"
+    pkgname=${package#*/}
+    pkgbuild="$package/PKGBUILD"
+    srcinfo="$package/.SRCINFO"
     eval "$(source "$pkgbuild"; echo locale="$_locale"; echo pkgver="$pkgver")"
 
-    [ -z "$pkgver" ] && error "Cannot get the current version of $package, ignoring." && continue
-    echo "Found the current version of $package: $pkgver"
-    [ "$pkgver" == "$version" ] && error "The current version of $package is the latest, ignoring." && continue
+    [ -z "$pkgver" ] && error "Cannot get the current version of $pkgname, ignoring." && continue
+    echo "Found the current version of $pkgname: $pkgver"
+    [ "$pkgver" == "$version" ] && error "The current version of $pkgname is the latest, ignoring." && continue
 
     sha512sum_i686=$(echo "$sha512sums" | awk "\$2 ~ /linux-i686\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
     sha512sum_x86_64=$(echo "$sha512sums" | awk "\$2 ~ /linux-x86_64\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
-    [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error "Get sha512sums of $package error, ignoring." && continue
+    [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error "Get sha512sums of $pkgname error, ignoring." && continue
     sed -i -E "s/pkgver=.+/pkgver='$version'/; s/sha512sums_i686=.+/sha512sums_i686=('$sha512sum_i686')/; s/sha512sums_x86_64=.+/sha512sums_x86_64=('$sha512sum_x86_64')/" "$pkgbuild"
 
     ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo" || continue
-    git commit -a -m "Update $package to $version" && git push
+    git -C "$package" commit -a -m "Update version: {$pkgver} -> {$version}" && git -C "$package" push
 done
+
+exit 0
