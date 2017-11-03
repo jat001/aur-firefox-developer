a8b4a667c738015b5341967cf60351f1d2237734
Jat <jat@sinosky.org>
2017-11-03 16:54:48 +0800

    add packages

---
+++ add.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# author: chat@jat.email
+
+function error () {
+    echo "$@" >&2
+}
+
+function quit () {
+    [ ${#@} -gt 0 ] && echo "$@"
+    exit 0
+}
+
+function die () {
+    [ ${#@} -gt 0 ] && error "$@"
+    exit 1
+}
+
+api_languages='https://product-details.mozilla.org/1.0/languages.json'
+api_versions='https://product-details.mozilla.org/1.0/firefox_versions.json'
+
+languages=$(curl -s $api_languages | jq -c 'to_entries[]' 2>/dev/null)
+version=$(curl -s $api_versions | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/null)
+
+[ -z "$languages" ] && die 'Cannot get available languages.'
+[ -z "$version" ] && die 'Cannot get the latest version.'
+echo "Found the latest version: $version"
+
+sha512sums=$(curl -s "https://download-installer.cdn.mozilla.net/pub/devedition/releases/$version/SHA512SUMS")
+[ -z "$sha512sums" ] && die 'Cannot get sha512sums.'
+
+workdir='/tmp/aur-firefox-developer'
+rm -fr "$workdir" && mkdir -p "$workdir"
+
+packages=()
+while read -r language; do
+    locale=$(echo "$language" | jq -r '.key')
+    language=$(echo "$language" | jq -r '.value.English')
+    echo "Found language: $language"
+
+    sha512sum_i686=$(echo "$sha512sums" | awk "\$2 ~ /linux-i686\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
+    sha512sum_x86_64=$(echo "$sha512sums" | awk "\$2 ~ /linux-x86_64\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
+    [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error 'Cannot get sha512sums, ignoring.' && continue
+
+    pkgname="firefox-developer-${locale,,}"
+    package="$workdir/$pkgname"
+    echo "New package: $pkgname"
+
+    aur_http="https://aur.archlinux.org/packages/$pkgname/"
+    aur_git="ssh://aur@aur.archlinux.org/$pkgname.git"
+    curl -fso /dev/null "$aur_http" && error 'Package exists, ignoring.' && continue
+
+    pkgbuild="$package/PKGBUILD"
+    srcinfo="$package/.SRCINFO"
+
+    (cp -r templates "$package" && sed -i "s/#locale#/$locale/; s/#language#/$language/; s/#pkgver#/$version/; s/#sha512sum_i686#/$sha512sum_i686/; s/#sha512sum_x86_64#/$sha512sum_x86_64/" "$pkgbuild" && ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo") || continue
+    (git -C "$package" init && git -C "$package" remote add origin "$aur_git" && git -C "$package" add -A && git -C "$package" commit -m "Initial version: {$version}" && git -C "$package" push -u origin master && git submodule add -b master "$aur_git" "packages/$pkgname") || continue
+
+    packages+=("$pkgname")
+done <<<"$languages"
+
+[ ${#packages[@]} -eq 0 ] && quit 'No package needs to add, exiting.'
+git commit -am "[skip ci] ${#packages[*]} new packages: ${packages[*]}" && git push
--- update.sh
+++ update.sh
@@ -15,15 +15,14 @@ function die () {
     exit 1
 }
 
-name='firefox-developer'
 api='https://product-details.mozilla.org/1.0/firefox_versions.json'
-version=$(curl $api 2>/dev/null | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/null)
+version=$(curl -s $api | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/null)
 
-[ -z "$version" ] && die "Cannot get the latest version of $name."
-echo "Found the latest version of $name: $version"
+[ -z "$version" ] && die 'Cannot get the latest version.'
+echo "Found the latest version: $version"
 
-sha512sums=$(curl "https://download-installer.cdn.mozilla.net/pub/devedition/releases/$version/SHA512SUMS" 2>/dev/null)
-[ -z "$sha512sums" ] && die "Cannot get sha512sums of $name."
+sha512sums=$(curl -s "https://download-installer.cdn.mozilla.net/pub/devedition/releases/$version/SHA512SUMS")
+[ -z "$sha512sums" ] && die 'Cannot get sha512sums.'
 
 git submodule update --init --remote --recursive || die
 
@@ -31,26 +30,26 @@ for package in packages/*; do
     git -C "$package" checkout master
 
     pkgname=${package#*/}
+    echo "Found package: $pkgname"
+
     pkgbuild="$package/PKGBUILD"
     srcinfo="$package/.SRCINFO"
 
     locale=$(source "$pkgbuild" && echo "$_locale")
     pkgver=$(source "$pkgbuild" && echo "$pkgver")
 
-    [ -z "$locale" ] && error "Cannot get the locale of $pkgname, ignoring." && continue
-    [ -z "$pkgver" ] && error "Cannot get the current version of $pkgname, ignoring." && continue
-    echo "Found the current version of $pkgname: $pkgver"
-    [ "$pkgver" == "$version" ] && error "The current version of $pkgname is the latest, ignoring." && continue
+    [ -z "$locale" ] && error 'Cannot get locale, ignoring.' && continue
+    [ -z "$pkgver" ] && error 'Cannot get current version, ignoring.' && continue
+    echo "Found current version: $pkgver"
+    [ "$pkgver" == "$version" ] && error 'Current version is the latest, ignoring.' && continue
 
     sha512sum_i686=$(echo "$sha512sums" | awk "\$2 ~ /linux-i686\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
     sha512sum_x86_64=$(echo "$sha512sums" | awk "\$2 ~ /linux-x86_64\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
-    [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error "Cannot get sha512sums of $pkgname, ignoring." && continue
+    [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error 'Cannot get sha512sums, ignoring.' && continue
 
-    sed -i -E "s/pkgver=.+/pkgver='$version'/; s/pkgrel=.+/pkgrel='1'/; s/sha512sums_i686=.+/sha512sums_i686=('$sha512sum_i686')/; s/sha512sums_x86_64=.+/sha512sums_x86_64=('$sha512sum_x86_64')/" "$pkgbuild"
-
-    ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo" || continue
-    git -C "$package" commit -a -m "Update version: {$pkgver} -> {$version}"
+    (sed -i -E "s/pkgver=.+/pkgver='$version'/; s/pkgrel=.+/pkgrel='1'/; s/sha512sums_i686=.+/sha512sums_i686=('$sha512sum_i686')/; s/sha512sums_x86_64=.+/sha512sums_x86_64=('$sha512sum_x86_64')/" "$pkgbuild" && ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo") || continue
+    git -C "$package" commit -am "Update version: {$pkgver} -> {$version}"
 done
 
 git diff --quiet && quit 'No package needs to update, exiting.'
-git commit -a -m "[skip ci] New version: {$version}" && git push --recurse-submodules=on-demand
+git commit -am "[skip ci] New version: {$version}" && git push --recurse-submodules=on-demand
