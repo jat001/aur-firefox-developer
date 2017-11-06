8dabc271391481c8a462ae8670eabd8f30def5be
Jat <jat@sinosky.org>
2017-11-06 11:49:05 +0800

    update

--- update.sh
+++ update.sh
@@ -15,9 +15,13 @@ function die () {
     exit 1
 }
 
-api='https://product-details.mozilla.org/1.0/firefox_versions.json'
-version=$(curl -s $api | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/null)
+api_languages='https://product-details.mozilla.org/1.0/languages.json'
+api_versions='https://product-details.mozilla.org/1.0/firefox_versions.json'
 
+languages=$(curl -s $api_languages)
+version=$(curl -s $api_versions | jq -r '.LATEST_FIREFOX_DEVEL_VERSION' 2>/dev/null)
+
+[ -z "$languages" ] && die 'Cannot get available languages.'
 [ -z "$version" ] && die 'Cannot get the latest version.'
 echo "Found the latest version: $version"
 
@@ -43,11 +47,14 @@ for package in packages/*; do
     echo "Found current version: $pkgver"
     [ "$pkgver" == "$version" ] && error 'Current version is the latest, ignoring.' && continue
 
+    language=$(echo "$languages" | jq -r ".[\"$locale\"].English")
+    [ -z "$language" ] && error "Unknown locale: $locale" && language="$locale"
+
     sha512sum_i686=$(echo "$sha512sums" | awk "\$2 ~ /linux-i686\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
     sha512sum_x86_64=$(echo "$sha512sums" | awk "\$2 ~ /linux-x86_64\/$locale\/firefox-${version}\.tar\.bz2/ { print \$1 }")
     [ -z "$sha512sum_i686" ] || [ -z "$sha512sum_x86_64" ] && error 'Cannot get sha512sums, ignoring.' && continue
 
-    (sed -i -E "s/pkgver=.+/pkgver='$version'/; s/pkgrel=.+/pkgrel='1'/; s/sha512sums_i686=.+/sha512sums_i686=('$sha512sum_i686')/; s/sha512sums_x86_64=.+/sha512sums_x86_64=('$sha512sum_x86_64')/" "$pkgbuild" && ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo") || continue
+    (find "$package" -mindepth 1 -maxdepth 1 -not -path "$package/.git" -exec rm -fr {} \; && cp -r templates/* "$package" && sed -i "s/#locale#/$locale/; s/#language#/$language/; s/#pkgver#/$version/; s/#sha512sum_i686#/$sha512sum_i686/; s/#sha512sum_x86_64#/$sha512sum_x86_64/" "$pkgbuild" && ./mksrcinfo.sh -p "$pkgbuild" -o "$srcinfo") || continue
     git -C "$package" commit -am "Update version: {$pkgver} -> {$version}"
 done
 
